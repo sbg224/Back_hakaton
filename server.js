40d154f9b37101fd6d0e76125198f6be5adfa614
sbg224 <sambah450@gmail.com>
2025-02-06 19:17:39 +0100

    fini config

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import RouteCategories from './RouteCategories.js';  // Importer les routes de c
 import RouteGrottes from './RouteGrottes.js';  // Importer les routes de grottes
 import RouteImages from './RouteImages.js'
 
+
 const app = express();
 
 // Middleware pour parser le corps des requêtes JSON
