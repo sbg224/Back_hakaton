371eb738c9d642fb905c24c74621fee5433920d5
Luigi-44 <luigi.rondeau@gmail.com>
2025-02-06 21:05:49 +0100

    localhost fix

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import express from 'express';
 import RouteCategories from './RouteCategories.js';  // Importer les routes de catégories
 import RouteGrottes from './RouteGrottes.js';  // Importer les routes de grottes
 import RouteImages from './RouteImages.js'
-
-
+import cors from 'cors';
 
 
 const app = express();
+app.use(cors());
+const port = process.env.PORT || 3000;
 
 // Middleware pour parser le corps des requêtes JSON
 app.use(express.json());
@@ -20,6 +21,6 @@ app.use('/api', RouteGrottes);  // Les grottes seront sous '/api/grottes'
 app.use('/api', RouteImages); 
 
 // Démarrer le serveur
-app.listen(3000, () => {
-  console.log('Serveur démarré sur le port 3000');
+app.listen(port, '0.0.0.0', () => {
+  console.log(`✅ Serveur démarré sur http://localhost:${port}`);
 });
